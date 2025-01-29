d4f8b46c5ca90a08a999f8b98a34e744a2c3ffb4
usr410 <elioande0@gmail.com>
2025-01-29 09:47:51 +0100

    -file word opgeslagen als pdf - gebruikers input toegevoegd

--- eerste_pdf.py
+++ eerste_pdf.py
@@ -1,9 +1,10 @@
 from reportlab.pdfgen import canvas
 
 def hallo(c):
-    c.drawString(100, 700, "Hallo dit is mijn eerste PDF.")
+    tekst = input("Voer een stuk tekst in. ")
+    c.drawString(100, 700, tekst)
 
-c = canvas.Canvas("Eerste PDF")
+c = canvas.Canvas("Eerste PDF.pdf")
 hallo(c)
 c.showPage()
 c.save()
